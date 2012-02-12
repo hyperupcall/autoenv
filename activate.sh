c523806a7f5f6973ec173d4ece99aa8309022f08
Kenneth Reitz <me@kennethreitz.com>
2012-02-12 07:42:22 -0500

    autoenv-init

--- activate.sh
+++ activate.sh
@@ -2,9 +2,7 @@
 
 DIR="$(dirname $0)"
 
-function cd(){
-    builtin cd "$@"
-
+function autoenv-init(){
     # Run detect script and split it up.
     saveIFS=$IFS; IFS=$'\n'; cmds=($($DIR/detect_env.py)); IFS=$saveIFS
 
@@ -12,4 +10,9 @@ function cd(){
     do
        eval $cmd
     done
+}
+
+function cd(){
+    builtin cd "$@"
+    autoenv-init
 }
