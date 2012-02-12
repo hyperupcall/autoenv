4b7a2c1359ad9d690ef542cc9a55cd4293ce44b7
Kenneth Reitz <me@kennethreitz.com>
2012-02-12 06:30:30 -0500

    .env

--- detect_env.py
+++ detect_env.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 import os
 
 
@@ -20,28 +19,12 @@ def find_above(*names):
 
 if __name__ == '__main__':
 
-    # Is there a .workon file?
-    wo_file = find_above('.env')
+    if 'AUTOENV_ACTIVE' not in os.environ:
 
-    # If there is, and we're not already in a virtualenv.
-    if wo_file and not 'VIRTUAL_ENV' in os.environ:
-        with open(wo_file) as f:
+        # Is there an .env file?
+        envfile = find_above('.env')
 
-            # Grab the venv path
-            venv_path = f.readlines()[0].strip()
-
-            # Activate!
-            print('source {0}/bin/activate'.format(venv_path))
-
-            # Mark it as ours.
-            print('export AUTOVIRTUALENV')
-
-    # If there's no file, bur we're in a virtualenv...
-    elif (not wo_file) and ('VIRTUAL_ENV' in os.environ.keys()):
-
-        # ... and if it's ours.
-        if '_ACTIVE' in os.environ:
-
-            # Deactivate the env.
-            print('deactivate')
-            print('unset AUTOVIRTUALENV')
+        if envfile:
+            # Do it live!
+            print('export AUTOENV_ACTIVE')
+            print('source %s' % envfile)
