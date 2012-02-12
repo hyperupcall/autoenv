84d6dfd3ffa823bf89ce63754a6ef10da51c334a
Jeremy Cantrell <jmcantrell@gmail.com>
2012-02-12 16:52:15 -0500

    safer quoting, resolve symlinks

--- activate.sh
+++ activate.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
-DIR="$(dirname $0)"
+DIR="$(dirname "$(readlink -f "$0")")"
 
 function autoenv-init(){
     # Run detect script and split it up.
     saveIFS=$IFS; IFS=$'\n'; cmds=($($DIR/detect_env.py)); IFS=$saveIFS
 
-    for cmd in ${cmds}
+    for cmd in "${cmds[@]}"
     do
        eval $cmd
     done
