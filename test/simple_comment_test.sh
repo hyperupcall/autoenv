48080620120945d81ee0384c686edb9d4356efe2
Michal Papis <mpapis@gmail.com>
2012-02-13 03:57:53 +0100

    add tests

---
+++ test/simple_comment_test.sh
@@ -0,0 +1,22 @@
+## setup
+source activate.sh
+
+test_dir="${TMPDIR:=/tmp}/_autoenv_$$"
+
+mkdir -p "$test_dir/a/b"
+echo "echo -a/b-" > "$test_dir/a/b/.env"
+echo "echo -a-" > "$test_dir/a/.env"
+mkdir -p "$test_dir/c/d"
+echo "echo -c/d-" > "$test_dir/c/d/.env"
+
+cd $test_dir
+
+## test
+( cd a   ) # match=/-a-/ ; status=0
+( cd a/b ) # match=/-a-/ ; match=/-a/b-/ ; status=0
+( cd c   ) # match=/$^/ ; status=0
+( cd c/d ) # match!=/-c-/ ; match=/-c/d-/ ; status=0
+( cd e   ) # match!=/^$/ ; status!=0
+
+## teardown
+rm -rf "$test_dir"
