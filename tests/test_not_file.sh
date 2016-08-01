934baa9b18fddb5eeee3389b66d189dff2d88460
Janne Heß <jannehess@gmail.com>
2016-08-01 10:52:53 +0200

    Add test for .env being a directory/fifo

---
+++ tests/test_not_file.sh
@@ -0,0 +1,15 @@
+. "${FUNCTIONS}"
+. "${ACTIVATE_SH}"
+
+#####
+# Tests what happens when .env is not a file.
+#####
+
+# Prepare files/directories
+mkdir -pv 'a/.env' 'b'
+mkfifo 'b/.env'
+
+# .env is a directory
+patterntest 'echo "Y" | cd a' '^$'
+# .env is a fifo
+patterntest 'echo "Y" | cd b' '^$'
