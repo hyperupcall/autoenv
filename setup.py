5dc43457f8b32812cd7bad56408de53e77b8fe93
inishchith <inishchith@gmail.com>
2019-07-25 22:30:55 +0530

    [setup] Update setup.py for PyPI release
    Signed-off-by: inishchith <inishchith@gmail.com>

--- setup.py
+++ setup.py
@@ -5,13 +5,21 @@
 import setuptools
 
 
+def readme():
+    with open("README.md") as f:
+        README = f.read()
+    return README
+
+
 setuptools.setup(
     version='0.2.1',
     name='autoenv',
     description='Directory-based environments.',
-    author='Kenneth Reitz',
-    author_email='_@kennethreitz.com',
+    long_description=readme(),
+    long_description_content_type="text/markdown",
+    author='Nishchith Shetty',
+    author_email='inishchith@gmail.com',
     license='See LICENSE.',
-    url='https://github.com/kennethreitz/autoenv',
+    url='https://github.com/inishchith/autoenv',
     scripts=['activate.sh'],
 )
