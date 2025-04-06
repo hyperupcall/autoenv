2b7e9baa07ba28fa183611d1820db14bc8f6bd23
Edwin Kofler <edwin@kofler.dev>
2025-04-06 02:57:11 -0700

    Tweaks

--- activate.sh
+++ activate.sh
@@ -1,4 +1,6 @@
 # shellcheck shell=sh
+# shellcheck disable=SC2216,SC3043
+
 if [ -n "$AUTOENV_AUTH_FILE" ]; then
 	:
 elif [ -f "$HOME/.autoenv_authorized" ]; then
@@ -14,10 +16,10 @@ fi
 if [ -n "$AUTOENV_NOTAUTH_FILE" ]; then
 	:
 elif [ -f "$HOME/.autoenv_authorized" ]; then
-	# If `.autoenv_authorized` is in home, don't suprise the user by using XDG Base Dir
+	# If `.autoenv_authorized` is in home, don't suprise the user by using XDG Base Dir.
 	AUTOENV_NOTAUTH_FILE="$HOME/.autoenv_not_authorized"
 elif [ -f "$HOME/.autoenv_not_authorized" ]; then
-	# Ensure file in ~/ is used, even if the authorized file has been moved or deleted
+	# Ensure file in ~/ is used, even if the authorized file has been moved or deleted.
 	AUTOENV_NOTAUTH_FILE="$HOME/.autoenv_not_authorized"
 else
 	_autoenv_state_dir="$XDG_STATE_HOME"
@@ -32,13 +34,11 @@ AUTOENV_ENV_LEAVE_FILENAME="${AUTOENV_ENV_LEAVE_FILENAME:-.env.leave}"
 AUTOENV_VIEWER="${AUTOENV_VIEWER:-cat}"
 # AUTOENV_ENABLE_LEAVE
 
-# @description print a user message to stdout
-# @args
-#   -b[NUM]: number of lines to print before message
-#   -a[NUM]: number of lines to print after message (default=1)
-#   -n: do not print trailing newline (same as -a0)
-#   message: space seperated text of message
-# @example _autoenv_info -n -b1 'my message'
+# @description Prints a user message to stdout
+# @option -b[NUM]: number of lines to print before message
+# @option -a[NUM]: number of lines to print after message (default=1)
+# @option -n: do not print trailing newline (same as -a0)
+# @arg message: space seperated text of message
 # @internal
 _autoenv_info() {
 	local after=1 before=0
@@ -64,10 +64,8 @@ _autoenv_info() {
 	[ $after -gt 0 ] && \printf '%*s' ${after} | \command tr " " "\n"
 }
 
-# @description print a message to stderr
-# @args
-#   message: space seperated text of message
-# @example _autoenv_err 'there was an error'
+# @description Prints a message to stderr
+# @args *: space seperated text of message
 # @internal
 _autoenv_err() {
 	if [ -n "$NO_COLOR" ]; then
@@ -79,10 +77,8 @@ _autoenv_err() {
 	\return 1
 }
 
-# @description print a horizontal line
-# @args
-#   text: title to print near the beginning of the line
-# @example _autoenv_draw_line 'text'
+# @description Prints a horizontal line
+# @args $1: title text to print near the beginning of the line
 # @internal
 _autoenv_draw_line() {
 	local text="${1}" char="-" width=${COLUMNS:-80} margin=3 line
@@ -101,12 +97,10 @@ _autoenv_draw_line() {
 fi
 }
 
-# @description display the contents of a `.env` or `.env.leave` file using the `$AUTOENV_VIEWER`` command
-# @example _autoenv_show_file './.env_file'
+# @description Displays the contents of a `.env` or `.env.leave` file using the `$AUTOENV_VIEWER` command
 # @internal
 _autoenv_show_file() {
 	local file="$1" ofs="$IFS"
-
 	_autoenv_info -b "New or modified env file detected:"
 	_autoenv_draw_line "${file##*/} contents"
 	IFS=" "
@@ -115,7 +109,7 @@ _autoenv_show_file() {
 	_autoenv_draw_line
 }
 
-# @description Main initialization function
+# @description This is executed on every `cd`
 # @internal
 autoenv_init() {
 	if [ -n "$AUTOENV_ENABLE_LEAVE" ]; then
@@ -183,11 +177,11 @@ ${_orderedfiles}"
 	fi
 }
 
-# @description Checks the hash
+# @description Checks the expected hash entry of the file
 # @internal
 autoenv_hashline() {
-	local _envfile _hash
-	_envfile="${1}"
+	local _envfile="${1}"
+	local hash
 	_hash=$(autoenv_shasum "${_envfile}" | \command cut -d' ' -f 1)
 	\printf '%s\n' "${_envfile}:${_hash}"
 }
@@ -195,8 +189,7 @@ autoenv_hashline() {
 # @description Source an env file if is able to do so
 # @internal
 _autoenv_check_authz_and_run() {
-	local _envfile="${1}"
-	local _hash
+	local _envfile="${1}" _hash
 	_hash=$(autoenv_hashline "${_envfile}")
 
 	\command mkdir -p -- "$(\command dirname "${AUTOENV_AUTH_FILE}")" "$(\command dirname "${AUTOENV_NOTAUTH_FILE}")"
@@ -230,8 +223,7 @@ _autoenv_check_authz_and_run() {
 # @description Mark an env file as able to be sourced
 # @internal
 autoenv_deauthorize_env() {
-	local _envfile _noclobber
-	_envfile="${1}"
+	local _envfile="${1}" _noclobber
 	\command cp -- "${AUTOENV_AUTH_FILE}" "${AUTOENV_AUTH_FILE}.tmp"
 	_noclobber="$(\set +o | \command grep noclobber)"
 	\set +C
@@ -251,8 +243,7 @@ autoenv_unauthorize_env() {
 # @description Mark an env file as able to be sourced
 # @internal
 autoenv_authorize_env() {
-	local _envfile
-	_envfile="${1}"
+	local _envfile="${1}"
 	autoenv_deauthorize_env "${_envfile}"
 	autoenv_hashline "${_envfile}" >> "${AUTOENV_AUTH_FILE}"
 }
@@ -273,8 +264,7 @@ autoenv_source() {
 
 # @description Function to override the 'cd' builtin
 autoenv_cd() {
-	local _pwd
-	_pwd=${PWD}
+	local _pwd=${PWD}
 	if \command -v chdir >/dev/null 2>&1 && \chdir "${@}" || \builtin cd "${@}"; then
 		autoenv_init "${_pwd}"
 		\return 0
@@ -286,8 +276,7 @@ autoenv_cd() {
 # @description Cleanup autoenv
 autoenv_leave() {
 	# execute file when leaving a directory
-	local from_dir to_dir
-	from_dir="${*}"
+	local from_dir="${*}" to_dir
 	to_dir=$(\echo "${PWD}" | \command sed -E 's:/+:/:g')
 
 	# Discover all files that we need to source.
